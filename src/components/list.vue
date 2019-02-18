f5ea526a305465057b862c1a6fd1702c9877258f
aafomin <aafomin@itps-russia.ru>
2019-02-18 12:35:37 +0500

    add localstarage

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h1>List {{ autos[0].id}}</h1>
+    <h1 @click.prevent="initState">List {{ autos[0].id}}</h1>
+    <!-- <h1 @click.prevent="addAuto"> {{ autos[0].id}}</h1> -->
+    <h1 @click.prevent="addLS"> {{ autos[0].name}}</h1>
+    <h1>ID {{ getID }}</h1>
     <span v-for="(auto, index) in autos" :key="index">
       {{ auto.name }}
     </span>
@@ -8,20 +11,44 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapMutations, mapGetters   } from 'vuex';
 
 export default {
   name: "List",
   components: {},
   data() {
     return {
+      auto: { id: 3, name: 'FORD'},
+      id: 3
+
       //
     };
   },
 computed: {
     ...mapState(
       'autoStore', ["autos"]
-    )
+    ),
+    ...mapGetters(
+      'autoStore', ["getById"]
+    ),
+    getID() {
+      return this.getById(this.id)
+    }
+},
+methods: {
+  ...mapMutations(
+      'autoStore', ['ADD_AUTOS', 'ADD_LS', 'INIT_STATE']
+    ),
+    addAuto() {
+      this.ADD_AUTOS(this.auto)
+    },
+    addLS() {
+      this.ADD_LS(this.auto)
+    },
+    initState() {
+      this.INIT_STATE
+      console.log(this.autos)
+    }
 }
 };
 </script>
--- src/store/autoStore.js
+++ src/store/autoStore.js
@@ -1,17 +1,34 @@
 export default {
   namespaced: true,
   state: {
-    autos: [
-      { id: 1, name: 'BMW',  },
-      { id: 2, name: 'Mercedes', }
-    ]
+    // autos: [
+    //   { id: 1, name: 'BMW'},
+    //   { id: 2, name: 'Mercedes'}
+    // ]
+    autos: JSON.parse(localStorage.getItem('autos'))
 
   },
   mutations: {
-
+    ADD_AUTOS: (state, auto) => {
+      state.autos.push(auto)
+    },
+    ADD_LS: (state, auto) => {
+      state.autos.push(auto)
+      localStorage.setItem('autos', JSON.stringify(state.autos))
+    },
+    INIT_STATE: (state) => {
+      if (localStorage.getItem('autos')) {
+        Object.assign(this.replaceState(state, JSON.parse(localStorage.getItem('autos'))))
+      }
+    }
   },
   actions: {
 
   },
+  getters: {
+    getById: state => (id) => {
+      return state.autos.find(autos => autos.id === id)
+    }
+  }
 }
 
