9a9c28f5449722f8e01dc0f7de2c94bda7c4942c
aafomin <aafomin@itps-russia.ru>
2019-02-21 10:05:22 +0500

    add mode history and list button loading style

--- src/components/auto/list.vue
+++ src/components/auto/list.vue
@@ -4,43 +4,47 @@
     <h1 @click.prevent="addLS">{{ autos[0].name}}</h1>
     <h1>ID {{ getID }}</h1>
     <span v-for="(auto, index) in autos" :key="index">{{ auto.name }}</span>-->
-          <v-flex xs12 sm8 offset-sm2>
-        <loading v-show="load"></loading>
-      </v-flex>
+    <v-flex xs12 sm8 offset-sm2>
+      <!-- <loading v-show="load"></loading> -->
+    </v-flex>
     <v-container style="background-color: #d8d4d4">
-
-      <v-flex xs12 sm8 md6>
-        <v-text-field label="Name" v-model="text" @change="validFields"></v-text-field>
-      </v-flex>
-      <v-flex xs12 sm8 md6>
-        <v-textarea
-          name="input"
-          label="Descriptions"
-          v-model="desc"
-          @change="validFields"
-          hint="Введите текст"
-        >{{ desc }}</v-textarea>
-      </v-flex>
-      <v-flex xs12 sm8 md6 class="text-xs-center text-sm-center text-md-center text-lg-center">
-        <img :src="imageUrl" height="150" v-if="imageUrl">
-        <v-text-field
-          label="Select Image"
-          @click="pickFile"
-          v-model="imageName"
-          prepend-icon="attach_file"
-        ></v-text-field>
-        <input
-          type="file"
-          style="display: none"
-          ref="image"
-          accept="image/*"
-          @change="onFilePicked"
-        >
-      </v-flex>
-      <v-btn fab dark color="indigo" :disabled="!valid" @click="upload">
-        <v-icon dark>add</v-icon>
-      </v-btn>
-      <v-btn color="indigo" @click="upload">Upload</v-btn>
+      <v-layout column>
+        <v-flex xs12 sm8 md6>
+          <v-text-field label="Name" v-model="text" @change="validFields"></v-text-field>
+        </v-flex>
+        <v-flex xs12 sm8 md6>
+          <v-textarea
+            name="input"
+            label="Descriptions"
+            v-model="desc"
+            @change="validFields"
+            hint="Введите текст"
+          >{{ desc }}</v-textarea>
+        </v-flex>
+        <v-flex xs12 sm8 md6 class="text-xs-center text-sm-center text-md-center text-lg-center">
+          <div v-for="(imageUrl, index) in imageUrls" :key="index">
+            <img :src="imageUrl" height="150" v-if="imageUrl">
+          </div>
+          <v-text-field
+            label="Select Image"
+            @click="pickFile"
+            v-model="imageName"
+            prepend-icon="attach_file"
+          ></v-text-field>
+          <input
+            type="file"
+            style="display: none"
+            ref="image"
+            accept="image/*"
+            @change="onFilePicked"
+          >
+        </v-flex>
+        <!-- <v-btn fab dark color="indigo" loading large :disabled="!valid" @click="upload"> -->
+        <v-btn fab dark color="indigo" :loading="load" large @click="upload" :disabled="!valid">
+          <v-icon dark>add</v-icon>
+        </v-btn>
+        <!-- <v-btn color="indigo" @click="testData">Upload</v-btn> -->
+      </v-layout>
     </v-container>
   </div>
 </template>
@@ -48,20 +52,22 @@
 <script>
 import { mapState, mapMutations, mapGetters, mapActions } from "vuex";
 // import firebase from 'firebase/app';
-import Loading from "@/views/loading.vue";
+// import Loading from "@/views/loading.vue";
 
 export default {
   name: "List",
   components: {
-    Loading
+    // Loading
   },
   data() {
     return {
       dialog: false,
       imageName: "",
       imageUrl: "",
+      imageUrls: [],
       imageFile: "",
       blobImage: "",
+      blobImages: [],
       desc: "",
       text: "",
       valid: false,
@@ -73,7 +79,7 @@ export default {
   computed: {
     ...mapState("autoStore", ["autos"]),
     ...mapGetters("autoStore", ["getById"]),
-    ...mapActions("autoStore", ['UPLOAD']),
+    ...mapActions("autoStore", ["UPLOAD"]),
     getID() {
       return this.getById(this.id);
     }
@@ -100,9 +106,17 @@ export default {
         const fr = new FileReader();
         fr.readAsDataURL(files[0]);
         fr.addEventListener("load", () => {
+          let arr = [];
           this.imageUrl = fr.result;
           this.imageFile = files[0];
-          this.blobImage = new Blob([files[0]], { type: "image/jpeg" });
+          this.arr.push(files[0]);
+          this.imageUrls.push(fr.result);
+          this.blobImages = new Blob([arr], { type: "image/jpeg" });
+
+          // this.blobImage = new Blob([files[0]], { type: "image/jpeg" });
+          // files.forEach(f => {
+          //   this.blobImage.push(f)
+          // })
         });
       } else {
         this.imageName = "";
@@ -128,20 +142,27 @@ export default {
     },
     upload() {
       this.load = true;
-      this.UPLOAD.then((res) => {
-        console.log(res)
-        this.$router.push("autoCard")
-      }).catch((err) => {
-        console.log(err)
+      this.UPLOAD.then(res => {
+        console.log(res);
+        this.$router.push("autoCard");
+      }).catch(err => {
+        console.log(err);
       });
+    },
+    testData() {
+      console.log(this.blobImage);
+      console.log(this.blobImages);
     }
   }
 };
 </script>
 
 <style lang="scss">
-.loading {
-    margin-top: 10%;
+.container {
+  max-width: 55%;
+  // .loading {
+  //   margin-top: 10%;
+  // }
 }
 </style>>
 
