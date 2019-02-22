eb561cd1b7316995faca16503ba0fb87de1dafe2
aafomin <aafomin@itps-russia.ru>
2019-02-22 23:05:32 +0500

    add transition in App router-view

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,54 @@
   <v-app>
     <app-header></app-header>
     <v-content transition="slide-x-transition">
-      <router-view/>
+      <transition name="fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import AppHeader from '@/components/AppHeader';
+import AppHeader from "@/components/AppHeader";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     AppHeader
   },
-  data () {
+  data() {
     return {
       //
-    }
+    };
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
+.fade-enter-active {
+  transition: all .5s ease;
+}
+.fade-leave-active {
+  transition: all .5s ease;
+}
+.fade-enter, .fade-leave-to {
+  transform: translateY(50px);
+  opacity: 0;
+}
+// .fade-enter {
+//   opacity: 0;
+// }
+
+// .fade-enter-active {
+//   transition: opacity 0.4s ease;
+// }
+
+// .fade-leave {
+// }
+
+// .fade-leave-active {
+//   transition: opacity 0.4s ease;
+//   opacity: 0;
+// }
 </style>
+
