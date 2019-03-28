ca602c6c353077c4eb3b3584622bff7629fe0cd9
aafomin <aafomin@parma-telecom.ru>
2019-03-29 01:24:48 +0600

    add multiple event to calendar

--- src/store/userDataDb.js
+++ src/store/userDataDb.js
@@ -17,10 +17,35 @@ export default {
   actions: {
     // eslint-disable-next-line
     async ADD_EVENT({ commit, dispatch }, payload) {
-      // await firebase.firestore().collection(payload.user.email).doc(payload.storage.id).collection("2").doc(payload.storage.id).set(payload.storage).then(function() {
-      await firebase.firestore().collection(payload.user.email).doc(payload.storage.id).set(payload.storage).then(function() {
-        // eslint-disable-next-line
-        console.log("Successfully written! - return Update Store");
+      let arrData = []
+      await firebase.firestore().collection(payload.user.email).doc(payload.storage.id).get().then(querySnapshot => {
+        if (querySnapshot.exists) {
+          querySnapshot.data().arrData.forEach(s => {
+            const data = s
+            const event = {
+              id: s.id,
+              day: data.day,
+              month: data.month,
+              year: data.year,
+              message: data.message
+            }
+            arrData.push(event)
+          })
+        }
+      })
+      const data = payload.storage;
+      const event = {
+        id: data.id,
+        day: data.day,
+        month: data.month,
+        year: data.year,
+        message: data.message
+      }
+      arrData.push(event)
+      let obj = {
+        arrData: arrData
+      }
+      await firebase.firestore().collection(payload.user.email).doc(payload.storage.id).set(obj).then(function() {
         dispatch("GET_EVENT", payload)
       })
       return "Success"
@@ -28,18 +53,22 @@ export default {
     async GET_EVENT({ commit }, payload) {
       let tempDB = []
       await firebase.firestore().collection(payload.user.email).get().then(querySnapshot => {
-        querySnapshot.forEach(s => {
-          const data = s.data()
-          let event = {
-            id: s.id,
-            day: data.day,
-            month: data.month,
-            year: data.year,
-            message: data.message
-          }
-          tempDB.push(event)
-        })
-        commit("SET_dataDb", tempDB)
+        if (querySnapshot) {
+          querySnapshot.docs.forEach(qS => {
+            qS.data().arrData.forEach(s => {
+              const data = s
+              let event = {
+                id: s.id,
+                day: data.day,
+                month: data.month,
+                year: data.year,
+                message: data.message
+              }
+              tempDB.push(event)
+            })
+            commit("SET_dataDb", tempDB)
+          })
+        }
       })
     },
     async ADD_FAVORITE_AUTO({ commit }, payload) {
