e9cfd01b8514826da7c5d6a0fbe954b51adb7e5c
aafomin <aafomin@itps-russia.ru>
2019-03-29 12:40:22 +0500

    fix bug cardItem

--- src/components/auto/cardItem.vue
+++ src/components/auto/cardItem.vue
@@ -1,45 +1,46 @@
 <template>
-  <v-container>
-      <listauto-item :auto="auto"></listauto-item>
+  <v-container v-if="dataReady">
+    <listauto-item :auto="auto"></listauto-item>
   </v-container>
 </template>
 
 <script>
-import { mapGetters, } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 import listautoItem from "./listAutoItem.vue";
-
 
 export default {
   name: "autoCard",
   components: {
     listautoItem
   },
-  // props: ['id']
   props: {
-    'id': {
+    id: {
       type: String,
       required: true
     }
   },
   data() {
     return {
+      dataReady: false
     };
   },
   mounted() {
+    this.LOAD_AUTO.then(r => {
+      this.dataReady = true;
+    });
   },
   computed: {
-     ...mapGetters("autoStore", ["getById"]),
-          auto() {
-       return this.getById(this.id)
-     }
+    ...mapActions("autoStore", ["LOAD_AUTO"]),
+    ...mapGetters("autoStore", ["getById"]),
+    auto() {
+      return this.getById(this.id);
+    }
     //  ...mapState("autoStore", ["autos"]),
     //  auto() {
     //    return this.autos.find( a => a.id === this.id)
     //  },
   },
-  methods: {
-  },
-
+  methods: {}
 };
 </script>
 
