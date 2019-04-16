96d01ffbc3b44b56b4e73936071258d2d6c7c056
aafomin <aafomin@itps-russia.ru>
2019-04-16 17:17:39 +0500

    change currency-exchange

--- src/components/Converter/currency-exchange.vue
+++ src/components/Converter/currency-exchange.vue
@@ -1,29 +1,56 @@
 <template>
   <div class="exchange">
     <v-layout wrap align-center>
-      <v-flex xs10 md4 sm10 offset-sm1>
-        <v-select
-          class="converter_select"
-          :items="dataCurrency"
+      <v-flex xs10 md4 sm10 offset-sm1 class="exchange__to">
+        <v-autocomplete
           v-model="currencyFrom"
-          item-text="name"
-          label="Конвертируем из"
-          return-object
-          @change="test"
-        ></v-select>
-      </v-flex>
-      <v-flex xs10 md4 sm10 offset-sm1>
-        <v-select
-          class="converter_select"
           :items="dataCurrency"
-          v-model="currencyTo"
+          label="Конвертируем из"
+          placeholder="Select..."
           item-text="name"
-          label="Конвертируем В"
+          item-value="name"
           return-object
-          @change="test"
-        ></v-select>
+          required
+        ></v-autocomplete>
+      </v-flex>
+      <v-flex xs10 md4 sm10 offset-sm1 v-show="currencyFrom != null" class="exchange__from">
+        <v-autocomplete
+          v-model="currencyTo"
+          :items="dataCurrency"
+          label="Конвертируем В"
+          placeholder="Select..."
+          item-text="name"
+          item-value="name"
+          return-object
+          required
+        ></v-autocomplete>
       </v-flex>
     </v-layout>
+    <v-layout wrap align-center>
+      <v-flex xs4 md4 sm4 offset-sm1 v-show="currencyTo != null" class="exchange__amound">
+        <v-text-field
+          :suffix='`${currencyFrom ? currencyFrom.symbol : ""}`'
+          type="number"
+          label="Amount"
+          v-model="amount"
+        ></v-text-field>
+      </v-flex>
+      <v-flex xs4 md4 sm4 offset-sm1 v-show="show" class="exchange__amound_converted">
+        <v-text-field
+          :suffix='`${currencyTo ? currencyTo.symbol : ""}`'
+          label="Amount Converted"
+          v-model="converted"
+          readonly
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-flex  xs4 md4 sm4 offset-sm1>
+    <v-btn color="orange" @click="convert" v-show="currencyTo">Конвертировать</v-btn>
+    </v-flex>
+    <v-flex xs4 md4 sm4 offset-sm1 v-if="show" class="exchange_result">
+    <p>{{ ` 1 ${currencyFrom.symbol} =  ${convertValue.val} ${currencyTo.symbol}` }}</p>
+    <p>{{ `${amount} ${currencyFrom.symbol} = ${converted} ${currencyTo.symbol}` }}</p>
+    </v-flex>
   </div>
 </template>
 <script>
@@ -36,9 +63,20 @@ export default {
     apiKey: key.currency,
     dataCurrency: [],
     currencyFrom: null,
-    currencyTo: null
+    currencyTo: null,
+    convertValue: {},
+    amount: 1,
+    searchInput: "",
+    show: false
   }),
-  computed: {},
+  computed: {
+    converted() {
+      let value = this.amount * this.convertValue.val;
+      let val = (value / 1).toFixed(2).replace(".", ",");
+      return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+      // return (this.amount * this.convertValue.val).toFixed(2)
+    }
+  },
   created() {
     this.init();
   },
@@ -50,6 +88,7 @@ export default {
       }`;
       axios.get(url).then(response => {
         let data = response.data.results;
+        console.log(response)
         let obj = {};
         let arr = [];
         Object.keys(data).forEach(key => {
@@ -64,17 +103,39 @@ export default {
         this.dataCurrency = arr;
       });
     },
-    test() {
-      // console.log(this.currencyFrom.id);
-      // console.log(this.currencyTo.id);
-    },
     convert() {
-      let url = `https://free.currencyconverterapi.com/api/v6/convert?apiKey=${this.apiKey}&q=USD_RUB`
+      let url = `https://free.currencyconverterapi.com/api/v6/convert?apiKey=${
+        this.apiKey
+      }&q=${this.currencyFrom.id}_${this.currencyTo.id}`;
+      axios.get(url).then(response => {
+        let obj = {};
+        let data = response.data.results;
+        Object.keys(data).forEach(key => {
+          let e = data[key];
+          obj = {
+            val: e.val,
+            from: e.fr,
+            to: e.to,
+            id: e.id
+          };
+          this.convertValue = obj;
+          this.show = true;
+        });
+      });
     }
   }
 };
 </script>
 <style lang="scss" scoped>
-@media screen and (max-width: 600px) {
+  .exchange {
+    background-color: #e1e1e1;
+    .exchange_result {
+      margin-top: 1%;
+    }
+  }
+@media screen and (max-width: 1000px) {
+  .exchange {
+    margin-left: 3vW;
+  }
 }
 </style>
