a7fb7d8ee16f6f9a2b527abecd2e69c80c34efd0
aafomin <aafomin@parma-telecom.ru>
2019-04-18 00:16:52 +0600

    add delete button in exchange

--- src/components/Converter/currency-exchange.vue
+++ src/components/Converter/currency-exchange.vue
@@ -11,7 +11,7 @@
           item-value="name"
           return-object
           required
-          @change="amount = 1"
+          @change="convert"
         ></v-autocomplete>
       </v-flex>
       <v-flex xs10 md4 sm10 offset-sm1 v-show="currencyFrom != null" class="exchange__from">
@@ -24,7 +24,7 @@
           item-value="name"
           return-object
           required
-          @change="amount = 1"
+          @change="convert"
         ></v-autocomplete>
       </v-flex>
     </v-layout>
@@ -46,15 +46,17 @@
         ></v-text-field>
       </v-flex>
     </v-layout>
-    <v-flex  xs4 md4 sm4 offset-sm1>
-    <v-btn color="orange" @click="convert" v-show="currencyTo">Конвертировать</v-btn>
-    <v-alert :value="errorMess" color="error" icon="warning" outline>{{  errorText }}.</v-alert>
+    <v-flex xs4 md4 sm4 offset-sm1>
+      <!-- <v-btn color="orange" @click="convert" v-show="currencyTo">Конвертировать</v-btn> -->
+      <v-alert :value="errorMess" color="error" icon="warning" outline>{{ errorText }}.</v-alert>
     </v-flex>
-    <v-flex  v-if="show" class="exchange_result">
-    <p class="text-xs-center">
-      {{ ` 1 ${currencyFrom.symbol} =  ${convertValue.val} ${currencyTo.symbol}` }}</p>
-    <p class="text-xs-center">
-      {{ `${amount} ${currencyFrom.symbol} = ${converted} ${currencyTo.symbol}` }}</p>
+    <v-flex v-if="show" class="exchange_result">
+      <p
+        class="text-xs-center"
+      >{{ ` 1 ${currencyFrom.symbol} = ${convertValue.val} ${currencyTo ? currencyTo.symbol : ""}` }}</p>
+      <p
+        class="text-xs-center"
+      >{{ `${amount} ${currencyFrom.symbol} = ${converted} ${currencyTo ? currencyTo.symbol : ""}` }}</p>
     </v-flex>
   </div>
 </template>
@@ -80,7 +82,7 @@ export default {
     converted() {
       let value = this.amount * this.convertValue.val;
       let val = (value / 1).toFixed(2).replace(".", ",");
-      return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+      return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
       // return (this.amount * this.convertValue.val).toFixed(2)
     }
   },
@@ -93,76 +95,81 @@ export default {
       let url = `https://free.currencyconverterapi.com/api/v6/currencies?apiKey=${
         this.apiKey
       }`;
-      axios.get(url)
+      axios
+        .get(url)
         .then(response => {
-        let data = response.data.results;
-        let obj = {};
-        let arr = [];
-        Object.keys(data).forEach(key => {
-          let e = data[key];
-          obj = {
-            name: e.currencyName,
-            symbol: e.currencySymbol ? e.currencySymbol : "",
-            id: e.id
-          };
-          arr.push(obj);
+          let data = response.data.results;
+          let obj = {};
+          let arr = [];
+          Object.keys(data).forEach(key => {
+            let e = data[key];
+            obj = {
+              name: e.currencyName,
+              symbol: e.currencySymbol ? e.currencySymbol : "",
+              id: e.id
+            };
+            arr.push(obj);
+          });
+          arr.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0));
+          this.dataCurrency = arr;
+        })
+        .catch(error => {
+          (this.errorMess = true),
+            (this.errorText = error.message),
+            // eslint-disable-next-line
+            console.error(error);
         });
-        arr.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
-        this.dataCurrency = arr;
-      })
-      .catch(error => {
-        this.errorMess = true,
-        this.errorText = error.message,
-        // eslint-disable-next-line
-        console.error(error)
-      })
     },
     convert() {
-      let url = `https://free.currencyconverterapi.com/api/v6/convert?apiKey=${
-        this.apiKey
-      }&q=${this.currencyFrom.id}_${this.currencyTo.id}`;
-      axios.get(url)
-      .then(response => {
-        let obj = {};
-        let data = response.data.results;
-        Object.keys(data).forEach(key => {
-          let e = data[key];
-          obj = {
-            val: e.val,
-            from: e.fr,
-            to: e.to,
-            id: e.id
-          };
-          this.convertValue = obj;
-          this.show = true;
-        });
-      })
-      .catch(error => {
-        this.errorMess = true,
-        this.errorText = error.message,
-        // eslint-disable-next-line
-        console.error(error)
-      })
+      if (this.currencyTo != null && this.currencyFrom != null) {
+        this.amount = 1;
+        let url = `https://free.currencyconverterapi.com/api/v6/convert?apiKey=${
+          this.apiKey
+        }&q=${this.currencyFrom.id}_${this.currencyTo.id}`;
+        axios
+          .get(url)
+          .then(response => {
+            let obj = {};
+            let data = response.data.results;
+            Object.keys(data).forEach(key => {
+              let e = data[key];
+              obj = {
+                val: e.val,
+                from: e.fr,
+                to: e.to,
+                id: e.id
+              };
+              this.convertValue = obj;
+              this.show = true;
+            });
+          })
+          .catch(error => {
+            (this.errorMess = true),
+              (this.errorText = error.message),
+              // eslint-disable-next-line
+              console.error(error);
+          });
+      }
     }
   }
 };
 </script>
 <style lang="scss" scoped>
-  .exchange {
-    width: 100%;
-    height: 100%;
-    .exchange_result {
-      margin-top: 1%;
-      font-size: 34px;
-      font-weight: 600;
-      line-height: 40px;
-    }
+.exchange {
+  width: 100%;
+  height: 100%;
+  .exchange_result {
+    margin-top: 1%;
+    font-size: 34px;
+    font-weight: 600;
+    line-height: 40px;
   }
+}
 @media screen and (max-width: 1000px) {
   .exchange {
-    margin-left: 3vW;
+    margin-left: 3vw;
     .exchange_result {
-      margin-top: 5vH;
+      margin-top: 5vh;
       font-size: 22px;
       line-height: 30px;
     }
