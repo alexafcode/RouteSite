eab8fe0801442fd09662d99ff413616710dbb342
aafomin <aafomin@itps-russia.ru>
2019-04-19 23:59:06 +0500

    change style and flex autoCard

--- src/components/auto/autoCard.vue
+++ src/components/auto/autoCard.vue
@@ -1,64 +1,79 @@
 <template>
-  <v-container>
-    <v-layout column class="card_filter">
-      <v-flex xs1 offset-sm0 v-for="(factory, index) in manufacturers" :key="index">
-        <v-checkbox v-model="checkedFactory" :value="factory" :label="`${factory.toString()}`"></v-checkbox>
-      </v-flex>
-      <div class="text-xs-left">
-        <v-btn round color="primary" dark @click.prevent="resetFilter">Сбросить</v-btn>
-      </div>
-    </v-layout>
-    <v-container>
-      <v-layout class="card_add">
-        <!-- <v-text-field label="Search" v-model="searchText"></v-text-field> -->
-        <v-btn fab dark color="brown darken-4" v-show="isAuthenticated" to="/list">
-          <v-icon dark>add</v-icon>
-        </v-btn>
-      </v-layout>
-      <v-container>
-        <v-layout row wrap justify-space-between>
-          <div class="card_list" v-for="(auto, index) in pagination" :key="index">
-            <listauto :auto="auto" @updateState="initState"></listauto>
+  <v-container fluid>
+    <loading v-show="!show"></loading>
+    <v-layout row>
+      <v-flex xs2 v-show="show" class="card_menu">
+        <v-layout class="card_add">
+          <!-- <v-text-field label="Search" v-model="searchText"></v-text-field> -->
+          <v-btn fab dark color="brown darken-4" v-show="isAuthenticated" to="/list">
+            <v-icon dark>add</v-icon>
+          </v-btn>
+        </v-layout>
+        <v-layout class="card_filter">
+          <v-flex xs1 offset-sm0 v-for="(factory, index) in manufacturers" :key="index">
+            <v-checkbox v-model="checkedFactory" :value="factory" :label="`${factory.toString()}`"></v-checkbox>
+          </v-flex>
+          <div class="text-xs-left">
+            <v-btn round color="primary" dark @click.prevent="resetFilter">Сбросить</v-btn>
           </div>
         </v-layout>
-      </v-container>
-      <v-layout row justify-center>
-        <div class="display-1 text-xs-center">
-          <p class="pages" @click="pageNumber = 1">Start</p>
-        </div>
-        <div class="text-xs-center">
-          <v-pagination v-model="pageNumber" :length="3" :total-visible="5" circle></v-pagination>
-        </div>
-        <div class="display-1 text-xs-center">
-          <p class="pages" @click="pageNumber = pageCount">End</p>
-        </div>
-      </v-layout>
-      <div class="text-xs-center">
-        <p>Всего страниц: <b>{{ pageCount }}</b></p>
+      </v-flex>
+      <div v-show="show">
+        <v-flex xs11>
+          <v-layout row wrap justify-space-between>
+            <div class="card_list" v-for="(auto, index) in pagination" :key="index">
+              <listauto :auto="auto" @updateState="initState"></listauto>
+            </div>
+          </v-layout>
+        </v-flex>
+        <v-flex xs12 md12 class="card_pagination">
+          <v-layout row justify-center>
+            <div class="text-xs-center">
+              <v-btn flat class="pages" @click="pageNumber = 1">Start</v-btn>
+            </div>
+            <div class="text-xs-center">
+              <v-pagination v-model="pageNumber" :length="3" :total-visible="5" circle></v-pagination>
+            </div>
+            <div class="text-xs-center">
+              <v-btn flat class="pages" @click="pageNumber = pageCount">End</v-btn>
+            </div>
+          </v-layout>
+          <div class="text-xs-center">
+            <p>
+              Всего страниц:
+              <b>{{ pageCount }}</b>
+            </p>
+          </div>
+        </v-flex>
       </div>
-    </v-container>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
 import { mapState, mapActions } from "vuex";
 import listauto from "./listAuto.vue";
+import loading from "../../views/loading.vue";
 
 export default {
   name: "autoCard",
   components: {
-    listauto
+    listauto,
+    loading
   },
   data() {
     return {
       searchText: null,
       pageNumber: 1,
       size: 4,
-      checkedFactory: []
+      checkedFactory: [],
+      show: false
     };
   },
   created() {
-    this.LOAD_AUTO;
+    this.LOAD_AUTO.then(() => {
+      this.show = true;
+    });
   },
   mounted() {},
   computed: {
@@ -129,32 +144,29 @@ export default {
 </script>
 <style lang="scss" scoped>
 .card_filter {
-  position: absolute;
-  left: 1%;
-  top: 12%;
+  flex-direction: column;
 }
-.card_add {
-  position: absolute;
-  left: 1%;
-  top: 1%;
+.pages {
+  font-size: 18px; //em
+  font-weight: 600;
 }
-
 .card_list {
   width: 50%;
 }
 @media screen and (max-width: 1024px) {
-  .card_filter {
-    flex-direction: row;
-    position: relative;
-    flex-wrap: wrap;
+  .card_menu {
+    display: none;
+    .card_filter {
+      flex-direction: row;
+    }
   }
+
   .card_list {
-    width: 100%;
+    width: 80%;
+    // margin-top: 12vh;
   }
-  .card_add {
-    position: absolute;
-    left: -1vw;
-    top: 19vh;
+  .card_pagination {
+    width: 90%;
   }
 }
 </style>
